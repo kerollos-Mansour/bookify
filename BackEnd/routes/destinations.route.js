2304f572a021f84aa067c351acfbba30f2689270
Mohamed Sabry <Mohamed.sabry.av@gmail.com>
2025-12-05 16:40:54 +0200

    Refactor destinations controller and routes for improved functionality
    - Updated destinations.controller.js to include an updateDestination method for modifying existing destinations.
    - Enhanced error handling and response formatting across all destination-related endpoints.
    - Modified destinations.route.js to add a PUT route for updating destinations.
    - Changed MongoDB connection string in .env to use local database for development.

--- BackEnd/routes/destinations.route.js
+++ BackEnd/routes/destinations.route.js
@@ -1,11 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createDestination, getAllDestinations, getDestinationById, deleteDestination } = require('../controller/destinations.controller');
+const {
+  createDestination,
+  getAllDestinations,
+  getDestinationById,
+  deleteDestination,
+  updateDestination,
+} = require("../controller/destinations.controller");
 
 // post new destination
-router.post('/', createDestination);
-router.get('/', getAllDestinations)
-router.get('/:id', getDestinationById)
-router.delete('/:id', deleteDestination)
+router.post("/", createDestination);
+router.get("/", getAllDestinations);
+router.get("/:id", getDestinationById);
+router.delete("/:id", deleteDestination);
+router.put("/:id", updateDestination);
 
 module.exports = router;
