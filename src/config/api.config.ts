270e8ae25ae8c615ec6b0cc4bd2be13192a6c30d
Mohamed Sabry <Mohamed.sabry.av@gmail.com>
2026-01-10 12:36:42 +0200

    api configurations

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1",
-  SOCKET_URL: import.meta.env.VITE_SOCKET_URL || "http://localhost:5000",
-  DASHBOARD_URL: import.meta.env.VITE_DASHBOARD_URL || "http://localhost:3000",
+  BASE_URL: import.meta.env.VITE_API_BASE_URL ,
+  SOCKET_URL: import.meta.env.VITE_SOCKET_URL ,
+  DASHBOARD_URL: import.meta.env.VITE_DASHBOARD_URL ,
   TIMEOUT: 10000,
 };
